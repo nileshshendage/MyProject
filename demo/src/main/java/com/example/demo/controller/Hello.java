ca16cf11e0796cc2450a3e80a94e891766028261
nileshs <nileshs@egovvtq09.egov.cdac.in>
2024-07-01 17:20:51 +0530

    MY FIRST COMMIT

--- demo/src/main/java/com/example/demo/controller/Hello.java
+++ demo/src/main/java/com/example/demo/controller/Hello.java
@@ -20,7 +20,7 @@ public class Hello {
 	@GetMapping("hello9")  
 	public String hello()   
 	{  
-	return "message fetch successfully";  
+	return "message fetch successfully...... NILESH";  
 	}  
 	
 	
@@ -28,7 +28,7 @@ public class Hello {
 	@GetMapping("G9")
 	public String myOwnMethod() 
 	{
-		return "I am now checking this with postman";
+		return "I am now checking this with postman.....";
 		
 	}
 	
